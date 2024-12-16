e989f800b1ac92317981eba2d1b235ffa8f34eb2
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2024-12-16 14:46:22 +1100

    Update Spells.js

--- Data/Spells.js
+++ Data/Spells.js
@@ -34,4 +34,30 @@ export const Spells = [
             }
         ]
     },
+    {
+        "Name" : "Hydrosnipe",
+        "Desc" : "Form a Bolt of Very Cold Water and Make A Ranged Attack Roll at An Enemy in Sight in order too Fling it at them. This Bolt of Water Deals 2d8 Cold and 2d8 Piercing Damage By Default, Interchangeably Raising by one Each Level (Level 3 is 3d8 Piercing + 2d8 Cold but Level 4 is 3d8 Piercing and 3d8 Cold)",
+        "Level" : "10",
+        "Excess Magic" : "100",
+        "Class" : "Wizard, Sorcerer",
+        "Duration" : "Instant",
+        "Casting Time" : "1 Action",
+        "School" : "Evocation",
+        "Range" : "120ft / 36m",
+        "Area" : "Point",
+        "Attack / Save" : "Ranged",
+        "Components" : "S",
+        "Damage" : [
+            {
+                "Type" : "Cold",
+                "Calculation" : "Math.floor(2+((SL-2)/2))",
+                "Dice" : "d8",
+            },
+            {
+                "Type" : "Piercing",
+                "Calculation" : "Math.ceil(2+((SL-2)/2))",
+                "Dice" : "d8",
+            }
+        ]
+    },
 ]
