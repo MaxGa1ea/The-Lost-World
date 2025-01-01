97d121d84dda306e9eeec1ef51050b96e4c7ec20
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2025-01-01 15:01:43 +1100

    added cult of the abyss

--- Data/Factions.js
+++ Data/Factions.js
@@ -3,7 +3,7 @@ export const Factions = [
         "Name" : "Icespire Alliance",
         "Colour" : "#44c9f4",
         "Image" : "Images/Factions/Icespire Alliance.png",
-        "Created" : "1510",
+        "Created" : "1510 PA",
         "Desc" : "A Small Alliance Set up by the Three Cities In the Icespire Peaks (Stormhold, Snowpoint, and Frostpeak) This Alliance Mostly exists as a way for Stormhold to Protect the Other Two Cities should they need it, and the Other Cities Providing Goods/Services such as Healthcare from Snowpoint and Food From Frostpeak",
         "Notable Person" : [
             {
@@ -54,6 +54,58 @@ export const Factions = [
                 "Title" : "Titan",
                 "Privileges" : "3 Bulwark Assistants (level 3 Chars) At Your Assistance"
             },
+        ]
+    },
+    {
+        "Name" : "Cult of the Abyss",
+        "Colour" : "#630091",
+        "Image" : "Images/Factions/Cult of the Abyss.png",
+        "Created" : "57 PU",
+        "Desc" : "The Cult of The Abyss is a cult that follows the Lady of Rot and Decay, Corvus. The Cult was established during the Chaos of the Purge and holds ties to mushrooms and myconids, commonly enslaving them. When lower-level cult members die, they are most likely turned into myconids themselves.",
+        "Notable Person" : [
+            {
+                "Type" : "Deities",
+                "Name" : "Corvus"
+            }
+        ],
+        "Notable Place" : [
+            {
+                "Type" : "Cities",
+                "Name" : "Drystam"
+            }
+        ],
+        "Notable Event" : [
+            {
+                "Type" : "Events",
+                "Name" : "The Great Shrooming",
+            }
+        ],
+        "Rewards" : [
+            {
+                "Favour" : "1",
+                "Title" : "Shroomling",
+                "Privileges" : "Access to Cult Hideouts"
+            },
+            {
+                "Favour" : "10",
+                "Title" : "Scourge",
+                "Privileges" : "Access to Cult Equipment"
+            },
+            {
+                "Favour" : "30",
+                "Title" :"Blight",
+                "Privileges" : "Necromancy Services"
+            },
+            {
+                "Favour" : "55",
+                "Title" : "Corrupter",
+                "Privileges" : "2 Myconid Adult Assistants",
+            },
+            {
+                "Favour" : "85",
+                "Title" : "Child of the Abyss",
+                "Privileges" : "Deep Dragon Wyrmling Mount"
+            }
         ]
     },
 ]
