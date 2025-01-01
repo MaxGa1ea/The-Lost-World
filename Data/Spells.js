96b94fefab4fe2df70d7d2f0bda68e1095dcb997
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2025-01-01 14:20:23 +1100

    removed lvl 10 hydrosnipe

--- Data/Spells.js
+++ Data/Spells.js
@@ -1,13 +1,3 @@
-/*
-<- BEFORE YOU START ->
-Hamlet: <20
-Village: 20-100
-Town: 100-1,000
-City: 1,000-10,000
-Large City: >10,000
-Capital: Largest In the Empire (Generally >20,000)
-*/
-
 export const Spells = [
     {
         "Name" : "Hydrosnipe",
@@ -34,7 +24,7 @@ export const Spells = [
             }
         ]
     },
-    {
+    /*{
         "Name" : "Hydrosnipe",
         "Desc" : "Form a Bolt of Very Cold Water and Make A Ranged Attack Roll at An Enemy in Sight in order too Fling it at them. This Bolt of Water Deals 2d8 Cold and 2d8 Piercing Damage By Default, Interchangeably Raising by one Each Level (Level 3 is 3d8 Piercing + 2d8 Cold but Level 4 is 3d8 Piercing and 3d8 Cold)",
         "Level" : "10",
@@ -59,5 +49,5 @@ export const Spells = [
                 "Dice" : "d8",
             }
         ]
-    },
+    },*/
 ]
